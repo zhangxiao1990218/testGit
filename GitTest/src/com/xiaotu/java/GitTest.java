ceb919634a36d0ac5dc43081db506fb95c1e2239
zhangxiao <zhangxiao@163.com>
2020-05-31 17:16:26 +0800

    Initial Commit

---
+++ GitTest/src/com/xiaotu/java/GitTest.java
@@ -0,0 +1,18 @@
+package com.xiaotu.java;
+
+/**
+ * @author 张晓
+ * @create 2020-05-30 11:11
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+    }
+
+}
